d53c2c1782c4d07adbac2f0028a3171518e14a50
jpimenteljose <jpimentel.jose@hotmail.com>
2026-04-23 18:58:38 -0300

    Criação de componentes, navegação em rotas e construção das páginas de dashboard, cadastro e consulta de clientes usando bootstrap.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { CadastrarClientes } from './pages/cadastrar-clientes/cadastrar-clientes';
+import { ConsultarClientes } from './pages/consultar-clientes/consultar-clientes';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'cadastrar-clientes',  // Rota
+        component: CadastrarClientes // Componente
+    },
+    {
+        path: 'consultar-clientes',  // Rota
+        component: ConsultarClientes // Componente
+    },
+    {
+        path: 'dashboard',   // Rota
+        component: Dashboard // Componente
+    },
+    {
+        path:'', pathMatch:'full', //raiz do projeto
+        redirectTo:'/dashboard'    //redirecionamento
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Navbar,
+    RouterOutlet
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
